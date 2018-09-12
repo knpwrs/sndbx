4b1fd8a1b09d9e99bd20d61f23a856201bf44d68
Kenneth Powers <ken@kenpowers.net>
2018-09-11 20:12:44 -0400

    js(fastify-hello): add fastify-hello example

---
+++ js/fastify/hello/index.js
@@ -0,0 +1,12 @@
+const fastify = require('fastify')({
+  logger: !!process.env.LOG,
+});
+
+fastify.get('/', (req, res) => {
+  res.send({ hello: 'world' });
+});
+
+fastify.listen(3000, (err, addr) => {
+  if (err) throw err;
+  fastify.log.info(`server listening on ${addr}`);
+});
