e5e99457410996395cf1cc2c47f30e4ff5ce0386
Kenneth Powers <ken@kenpowers.net>
2018-09-11 21:14:28 -0400

    rust(iron-hello): add iron-hello example

---
+++ rust/iron-hello/src/main.rs
@@ -0,0 +1,21 @@
+extern crate iron;
+extern crate rustc_serialize;
+
+use iron::prelude::*;
+use iron::status;
+use iron::mime::Mime;
+use rustc_serialize::json;
+
+#[derive(RustcEncodable)]
+struct JsonResponse {
+    hello: String,
+}
+
+fn main() {
+    Iron::new(|_: &mut Request| {
+        let content_type = "application/json".parse::<Mime>().unwrap();
+        let response = JsonResponse { hello: String::from("world") };
+        let out = json::encode(&response).unwrap();
+        Ok(Response::with((content_type, status::Ok, out)))
+    }).http("localhost:3000").unwrap();
+}
