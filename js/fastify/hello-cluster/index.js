6022f0ab7a882d5e47b59b6d2efc550eb362cd37
Kenneth Powers <ken@kenpowers.net>
2018-09-11 20:21:15 -0400

    js(fastify-hello-cluster): add fastify-hello-cluster example

---
+++ js/fastify/hello-cluster/index.js
@@ -0,0 +1,26 @@
+const cluster = require('cluster');
+
+if (cluster.isMaster) {
+  const numCpus = require('os').cpus().length;
+
+  for (let i = 0; i < numCpus; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died (code ${code}, signal ${signal})`);
+  });
+} else {
+  const fastify = require('fastify')({
+    logger: !!process.env.LOG,
+  });
+
+  fastify.get('/', (req, res) => {
+    res.send({ hello: 'world' });
+  });
+
+  fastify.listen(3000, (err, addr) => {
+    if (err) throw err;
+    fastify.log.info(`server listening on ${addr}`);
+  });
+}
